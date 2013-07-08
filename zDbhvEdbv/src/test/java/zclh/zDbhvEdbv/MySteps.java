1757f85957f62b0fc489c096ab631d2d2464e50a
alissoncoelhodemorais <alisson.coelho.de.morais@gmail.com>
2013-07-08 14:54:42 -0300

    Primeira versão.

---
+++ zDbhvEdbv/src/test/java/zclh/zDbhvEdbv/MySteps.java
@@ -0,0 +1,7 @@
+package zclh.zDbhvEdbv;
+
+import br.gov.frameworkdemoiselle.behave.parser.Step;
+
+public class MySteps implements Step {
+
+}
---
+++ zDbhvEdbv/src/test/java/zclh/zDbhvEdbv/MyTest.java
@@ -0,0 +1,21 @@
+package zclh.zDbhvEdbv;
+
+import org.junit.Test;
+
+import br.gov.frameworkdemoiselle.behave.controller.BehaveController;
+
+public class MyTest {
+
+	private BehaveController eng = null;
+
+	public MyTest() {
+		eng = BehaveController.getInstance();
+		eng.addSteps(new MySteps());
+	}
+
+	@Test
+	public void testAllStories() throws Throwable {
+		eng.run("/stories");
+	}
+
+}
